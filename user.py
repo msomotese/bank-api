945217ef07dac584e42b271ee7af3aa84a028652
Oyenbhen Omotese <141953502+msomotese@users.noreply.github.com>
2025-08-06 11:39:36 -0700

    Update user.py
    solution

--- user.py
+++ user.py
@@ -13,21 +13,22 @@ class BankUser:
         """
         TODO: complete this function that adds the current user to the bank database
         """
-        ...
+        bank.create_user(self.user_name, self.__password)
     
     def check_balance(self):
         """
         TODO: complete this function that returns the balance of the current user
         """
-        return ...
+        return self.bank.get_balance(self.user_name, self.__password)
     
     def deposit(self, amount):
         """
         TODO: complete this function that deposits money into the current users account
         """
-        ...
+        self.bank.deposit(self.user_name, self.__password, amount)
     
     def withdraw(self, amount):
         """
         TODO: complete this function that 
         """
+        self.bank.withdraw(self.user_name, self.__password, -1 * amount)
